bddb1e9613173c1910e14778b9bfb0b1ac1d097a
Yunus <yunusozdemir468@gmail.com>
2021-04-23 16:22:01 +0300

    detail.component.html

--- src/app/components/user/user-detail/user-detail.component.ts
+++ src/app/components/user/user-detail/user-detail.component.ts
@@ -1,15 +1,107 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router"; // ActivatedRoue is used to get the current associated components information.
+import { Location } from "@angular/common"; // Location service is used to go back to previous component
+import { ToastrService } from "ngx-toastr";
+import { UserService } from "src/app/services/user.service";
+import { User } from "src/app/models/user";
 @Component({
-  selector: 'app-user-detail',
-  templateUrl: './user-detail.component.html',
-  styleUrls: ['./user-detail.component.css']
+  selector: "app-user-detail",
+  templateUrl: "./user-detail.component.html",
+  styleUrls: ["./user-detail.component.css"],
 })
 export class UserDetailComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  editForm: FormGroup;
+  title = "Create";
+  errorMessage: any;
+  user = new User();
+  userId: string;
+  constructor(
+    private userService: UserService,
+    private fb: FormBuilder,
+    private location: Location,
+    private actRoute: ActivatedRoute,
+    private router: Router,
+    private toastr: ToastrService
+  ) {
+    if (this.actRoute.snapshot.params["id"]) {
+      this.userId = this.actRoute.snapshot.paramMap.get("id") || "id gelmiyor";
+      console.log(this.userId);
+    }
   }
 
+  ngOnInit(): void {
+    this.updateUserData();
+
+    if (this.userId) {
+      this.title = "Edit";
+      this.userService.getUserById(this.userId).subscribe((result: any) => {
+        if (result) {
+          this.user = result;
+          this.user.name = result.name;
+          console.log(result);
+        }
+      });
+    }
+  }
+
+  get name() {
+    return this.editForm.get("name");
+  }
+  get email() {
+    return this.editForm.get("email");
+  }
+  get photoURL() {
+    return this.editForm.get("photoURL");
+  }
+  get petName() {
+    return this.editForm.get("petName");
+  }
+
+  updateUserData() {
+    this.editForm = this.fb.group({
+      name: ["this.user.name", [Validators.required, Validators.minLength(2)]],
+      email: [
+        this.user.email,
+        [
+          Validators.required,
+          Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$"),
+        ],
+      ],
+      photoURL: [this.user.photoURL],
+      petName: [this.user.petName],
+    });
+  }
+
+  goBack() {
+    this.router.navigate(["/general-page"]);
+  }
+
+  onUserFormSubmit() {
+    if (this.userId) {
+      this.userService.update(this.userId, this.editForm.value).then(() => {
+        this.toastr.success(
+          this.editForm.controls["name"].value + " başarıyla güncelleştirildi"
+        );
+        this.router.navigate(["/view-users"]);
+      });
+    } else {
+      this.userService.saveUser(this.user).then(() => {
+        this.router.navigate(["/view-users"]);
+      });
+    }
+  }
+
+  getUser(user: string) {
+    this.userId = user;
+    this.userService.getUserById(user).subscribe((response: any) => {
+      this.editForm.patchValue({
+        id: response.data.id,
+        name: response.data.name,
+        photoURL: response.data.photoURL,
+        email: response.data.email,
+      });
+      console.log(this.userId);
+    });
+  }
 }
