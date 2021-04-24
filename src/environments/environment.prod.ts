4dc87da6ee61ed80558f90858ebe0e7cabcd72b6
Yunus <yunusozdemir468@gmail.com>
2021-04-24 15:26:44 +0300

    for deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBMqhI4wqhYMtAvr7jEcI75DfXsh78B-xc",
+    authDomain: "petproject-5f88b.firebaseapp.com",
+    databaseURL: "https://petproject-5f88b-default-rtdb.firebaseio.com",
+    projectId: "petproject-5f88b",
+    storageBucket: "petproject-5f88b.appspot.com",
+    messagingSenderId: "410913759097",
+    appId: "1:410913759097:web:21c158186786ce9f5b59a8",
+    measurementId: "G-NM40FSFSPX",
+  },
 };
